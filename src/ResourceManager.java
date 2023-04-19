37f1c94009a00e17afc552af0013dfa41c7fc344
viorelstoica1 <viorel.stoica@student.tuiasi.ro>
2023-04-20 02:17:18 +0300

    cursor pe culori, adaugat texturi culori

--- src/ResourceManager.java
+++ src/ResourceManager.java
@@ -4,43 +4,96 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Vector;
 
 public class ResourceManager {
-    private final HashMap<String,Textura> texturi;
+    private final Vector<Textura> texturiBile, texturiCursorPrincipal, texturiCursorSecundar;
     private final ArrayList<String> nume;
     public ResourceManager(){
         try {
-            texturi = new HashMap<>();
+            texturiBile = new Vector<>();
+            texturiCursorPrincipal = new Vector<>();
+            texturiCursorSecundar = new Vector<>();
             nume = new ArrayList<String>();
+            //galben
             BufferedImage tex = ImageIO.read(new File("src/resources/BILE_GALBENE.png"));
-            texturi.put("galben",new Spritesheet(tex,60,10,0,0,0));
+            texturiBile.add(new Spritesheet(tex,60,10,0,0,0));
+            tex = ImageIO.read(new File(("src/resources/cursor/cursor_galben.png")));
+            texturiCursorPrincipal.add( new Textura(tex,0,0,0).resize(36,36));
+            tex = ImageIO.read(new File(("src/resources/cursor/craniu_galben.png")));
+            texturiCursorSecundar.add( new Textura(tex,0,0,0).resize(36,36));
             nume.add("galben");
+            //albastru
             tex = ImageIO.read(new File("src/resources/BILE_ALBASTRE.png"));
-            texturi.put("albastru",new Spritesheet(tex,60,10,0,0,0));
+            texturiBile.add(new Spritesheet(tex,60,10,0,0,0));
+            tex = ImageIO.read(new File(("src/resources/cursor/cursor_albastru.png")));
+            texturiCursorPrincipal.add( new Textura(tex,0,0,0).resize(36,36));
+            tex = ImageIO.read(new File(("src/resources/cursor/craniu_albastru.png")));
+            texturiCursorSecundar.add( new Textura(tex,0,0,0).resize(36,36));
             nume.add("albastru");
+            //alb
             tex = ImageIO.read(new File("src/resources/BILE_ALBE.png"));
-            texturi.put("alb",new Spritesheet(tex,60,10,0,0,0));
+            texturiBile.add(new Spritesheet(tex,60,10,0,0,0));
+            tex = ImageIO.read(new File(("src/resources/cursor/cursor_alb.png")));
+            texturiCursorPrincipal.add( new Textura(tex,0,0,0).resize(36,36));
+            tex = ImageIO.read(new File(("src/resources/cursor/craniu_alb.png")));
+            texturiCursorSecundar.add( new Textura(tex,0,0,0).resize(36,36));
             nume.add("alb");
+            //mov
             tex = ImageIO.read(new File("src/resources/BILE_MOV.png"));
-            texturi.put("mov",new Spritesheet(tex,60,10,0,0,0));
+            texturiBile.add(new Spritesheet(tex,60,10,0,0,0));
+            tex = ImageIO.read(new File(("src/resources/cursor/cursor_mov.png")));
+            texturiCursorPrincipal.add( new Textura(tex,0,0,0).resize(36,36));
+            tex = ImageIO.read(new File(("src/resources/cursor/craniu_mov.png")));
+            texturiCursorSecundar.add( new Textura(tex,0,0,0).resize(36,36));
             nume.add("mov");
+            //rosu
             tex = ImageIO.read(new File("src/resources/BILE_ROSII.png"));
-            texturi.put("rosu",new Spritesheet(tex,60,10,0,0,0));
+            texturiBile.add(new Spritesheet(tex,60,10,0,0,0));
+            tex = ImageIO.read(new File(("src/resources/cursor/cursor_rosu.png")));
+            texturiCursorPrincipal.add( new Textura(tex,0,0,0).resize(36,36));
+            tex = ImageIO.read(new File(("src/resources/cursor/craniu_rosu.png")));
+            texturiCursorSecundar.add( new Textura(tex,0,0,0).resize(36,36));
             nume.add("rosu");
+            //verde
             tex = ImageIO.read(new File("src/resources/BILE_VERZI.png"));
-            texturi.put("verde",new Spritesheet(tex,60,10,0,0,0));
+            texturiBile.add(new Spritesheet(tex,60,10,0,0,0));
+            tex = ImageIO.read(new File(("src/resources/cursor/cursor_verde.png")));
+            texturiCursorPrincipal.add( new Textura(tex,0,0,0).resize(36,36));
+            tex = ImageIO.read(new File(("src/resources/cursor/craniu_verde.png")));
+            texturiCursorSecundar.add( new Textura(tex,0,0,0).resize(36,36));
             nume.add("verde");
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-    public Textura getTextura(String numeTextura){
-        return texturi.get(numeTextura);
-    }
+
     public Textura getBilaRandom(){
-        return texturi.get(nume.get((int)(Math.random() * 3/*nume.size()*/)));
+        return texturiBile.get((int) (Math.random()*nume.size()));
     }
 
+    public Textura getTexturaCursorPrincipal(Textura bila){
+        for(Textura textura: texturiBile){
+            if(textura.GetTex() == bila.GetTex()){
+                return texturiCursorPrincipal.get(texturiBile.indexOf(textura));
+            }
+        }
+        return null;
+    }
+    public Textura getTexturaCursorPrincipal(){
+        return texturiCursorPrincipal.get(0);
+    }
+    public Textura getTexturaCursorSecundar(){
+        return texturiCursorSecundar.get(0);
+    }
+
+    public Textura getTexturaCursorSecundar(Textura bila){
+        for(Textura textura: texturiBile){
+            if(textura.GetTex() == bila.GetTex()){
+                return texturiCursorSecundar.get(texturiBile.indexOf(textura));
+            }
+        }
+        return null;
+    }
 }
